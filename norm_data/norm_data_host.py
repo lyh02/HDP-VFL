ee715279653b7a23883f7052657c4067b1f9d530
XtangEver <60846307+XtangEver@users.noreply.github.com>
2021-10-20 19:22:12 +0800

    Update norm_data_host.py

--- norm_data/norm_data_host.py
+++ norm_data/norm_data_host.py
@@ -1,60 +1,38 @@
-import  copy
-import numpy as np
-
 from federatedml.model_base import ModelBase
-from federatedml.transfer_variable.transfer_class.norm_data_transfer_variable import NormDataTransferVariable
-from federatedml.util import consts
 from federatedml.util import LOGGER
-from federatedml.util import abnormal_detection
+from federatedml.param.norm_test_param import NormTestParam
+from federatedml.transfer_variable.transfer_class.norm_test_transfer_variable import NormTestTransferVariable
+from federatedml.util import consts
 from federatedml.feature.instance import Instance
-from federatedml.param.norm_data_param import NormDataParam
 
-class NormDataHost(ModelBase):
+class NormTestHost(ModelBase):
     def __init__(self):
-        super(NormDataHost, self).__init__()
-        self.transfer_variable = NormDataTransferVariable()
-        self.model_param = NormDataParam()
-        self.norm_x = None
+        super(NormTestHost, self).__init__()
+        # 赋值参数模型
+        self.model_param = NormTestParam()
+        # 传输变量赋值
+        self.transfer_variable = NormTestTransferVariable()
 
-    def _abnormal_detection(self,data_instances):
-        """检查输入的数据是否有效"""
-        abnormal_detection.empty_table_detection(data_instances)
-        abnormal_detection.empty_feature_detection(data_instances)
-        ModelBase.check_schema_content(data_instances.schema)
+    def fit(self,data_instances):
+        LOGGER.info("开始生成最大值和最小值数据表")
+        LOGGER.info("查看x.features的数据类型:{}".format(type(data_instances.first()[1].features)))
+        min_max_host = data_instances.mapValues(lambda x : (x.features.min(),x.features.max()))
 
-    @staticmethod
-    def load_data(data_instance):
-        """
-        设置数据标签为1和-1
-        """
-        data_instance = copy.deepcopy(data_instance)
-        if data_instance.label != 1:
-            data_instance.label = -1
-        return data_instance
+        LOGGER.info("将min_max_host发送给guest")
+        self.transfer_variable.host_to_guest.remote(obj=min_max_host,role=consts.GUEST,idx=-1,suffix=("host_to_guest",))
 
-    def fit(self, data_inst):
-        """
-        开始正式处理数据
-        """
-        self._abnormal_detection(data_inst)
-        data_instances = data_inst.mapValues(self.load_data)
+        LOGGER.info("接收min_max_global的值")
+        min_max_global = self.transfer_variable.guest_to_host.get(idx=-1,suffix=("guest_to_host",))
 
         LOGGER.info("开始归一化数据")
-        LOGGER.info("开始计算sum_square_x_host的结果")
-        sum_square_x_host = data_instances.mapValues(lambda x : np.sum(np.power(x.features,2)))
+        self.norm_data = data_instances.join(min_max_global[0],
+                                             lambda x, y: Instance(features=(x.features - y[0]) / (y[1] - y[0])))
 
-        LOGGER.info("将sum_square_x_host发送给guest方")
-        self.transfer_variable.host_to_guest.remote(obj=sum_square_x_host,role=consts.GUEST,idx=-1,suffix=(0,0))
-
-        LOGGER.info("从guest方接收结果norm_x")
-        self.norm_x = self.transfer_variable.guest_to_host.get(idx=-1,suffix=(1,1))
-
-        LOGGER.info("开始归一化数据")
-        self.data_output = data_inst.join(self.norm_x[0],lambda x,y : Instance(features=np.true_divide(x.features,y),label=x.label))
-        return self.data_output
+        return self.norm_data
 
     def save_data(self):
-        return self.data_output
+        return self.norm_data
+
 
 
 
